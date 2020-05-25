bb514ed30df139f4dcc1e673b7c89cfeb0cd616b
Paolo <paolomerlino98@gmail.com>
2020-05-25 16:20:35 +0200

    aggiunto viewcontroller per il minigame della vista

---
+++ NewMaze/Sight/GameViewController.swift
@@ -0,0 +1,51 @@
+//
+//  GameViewController.swift
+//  MinigameSight
+//
+//  Created by Paolo Merlino on 04/05/2020.
+//  Copyright © 2020 PaoloMerlino. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+
+class GameViewController: UIViewController {
+    
+
+    
+    override func viewDidLoad() {
+
+
+        super.viewDidLoad()
+
+        let scene = SightGame(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+        
+    }
+
+    override var shouldAutorotate: Bool {
+        return false
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    
+    
+}
