c446eb8b5e6d2059c24b84ffb98ad9f6c5481065
adrianog21 <adrianogatto@hotmail.it>
2020-05-28 14:26:47 +0200

    velocità

--- NewMaze/Menu/MenuScene.swift
+++ NewMaze/Menu/MenuScene.swift
@@ -25,14 +25,14 @@ class MenuScene: SKScene {
         for i in 0 ... 3 {
             let background = SKSpriteNode(texture: texture)
             background.anchorPoint = .zero
-            background.alpha = 0.3
+            background.alpha = 0.5
             background.size = CGSize(width: texture.size().width * 2, height: texture.size().height * 2)
             let start = CGPoint(x: (((background.size.width ) * CGFloat(i)) - 900), y: -180)
             background.position = start
             
             addChild(background)
             
-        let moveleft = SKAction.moveTo(x: (start.x - background.size.width), duration: 72)
+        let moveleft = SKAction.moveTo(x: (start.x - background.size.width), duration: 90)
         let movereset = SKAction.moveTo(x: start.x, duration: 0)
             let loop = SKAction.sequence([moveleft, movereset])
             let moverepeat = SKAction.repeatForever(loop)
