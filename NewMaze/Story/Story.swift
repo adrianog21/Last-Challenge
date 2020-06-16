00e053c0eeab6ed856c70ea6203b32b1a8ad13d9
Martina Dinardo <martinad.17@hotmail.com>
2020-06-16 03:40:28 +0200

    story

--- NewMaze/Story/Story.swift
+++ NewMaze/Story/Story.swift
@@ -21,14 +21,14 @@ class Story : UIViewController{
     var start2 = "Don’t despair, \nthis is your chance to acquire your human vitality back \nbefore irreversibly sinking into the darkness."
     var start3 = "I am going to light up the way, \nfollowing your every direction. \nThis is going to feel like a maze..."
     var start4 = "It is."
-    var start5 = "I wonder… \nSlits of light may still be open. \nfind them and try to force your senses back"
+    var start5 = "I wonder… \nSlits of light may still be open. \nFind them and try to force your senses back"
     
     var firstGameDone1 = "You are starting to feel your senses again… \nIt may feel frightening at first."
     var firstGameDone2 = "However you still need more to be free to live again. \nContinue your journey."
     
     var allGamesDone1 = "You have never felt this human in a long time. \nAnd I am starting to fade back into your consciousness…"
     var allGamesDone2 = "Time is killer now. \nHere’s my last advice…"
-    var allGamesDone3 = "Now you are able to feel the presence of three pulses. \nBefore they were unreachable for your senseless essence."
+    var allGamesDone3 = "Now you are able to feel the presence of three pulses. \nBefore, they were unreachable for your senseless essence."
     var allGamesDone4 = "Find them and find your way out \nHurry with the last help I can offer!"
     
     var firstFail1 = "Seems like you were not stable enough to withstand the impact of acquiring this sense back yet."
